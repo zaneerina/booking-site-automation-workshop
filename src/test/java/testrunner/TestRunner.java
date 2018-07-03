5197a58285040b8560fe4d6be79e0bf5921c8f54
Zane <zane.erina@testdevlab.com>
2018-07-03 18:48:03 +0300

    added

---
+++ src/test/java/testrunner/TestRunner.java
@@ -0,0 +1,13 @@
+package testrunner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"stepdefinitions"} //norāda ceļu uz step definitions failiem
+)
+public class TestRunner {
+}
